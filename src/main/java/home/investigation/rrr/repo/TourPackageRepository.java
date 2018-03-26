4f46bd04dd979da384e658964f454cd87e9812b5
RRRLuxoft <Ehf!23456>
2018-03-27 00:23:26 +0200

    Control API expose // exmp: http://localhost:8080/packages/CC

--- src/main/java/home/investigation/rrr/repo/TourPackageRepository.java
+++ src/main/java/home/investigation/rrr/repo/TourPackageRepository.java
@@ -3,9 +3,36 @@ package home.investigation.rrr.repo;
 import home.investigation.rrr.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     Optional<TourPackage> findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
