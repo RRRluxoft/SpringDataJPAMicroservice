b29f6040efa4945235495cb369ca4aa2578b4e72
RRRLuxoft <Ehf!23456>
2018-03-26 23:01:06 +0200

    sorting and paging // exmpl: http://localhost:8080/tours/search/findByTourPackageCode?code=CC&size=1&page=2&sort=title,desc http://localhost:8080/tours?size=3&page=1&sort=title,asc

--- src/main/java/home/investigation/rrr/repo/TourRepository.java
+++ src/main/java/home/investigation/rrr/repo/TourRepository.java
@@ -3,9 +3,9 @@ package home.investigation.rrr.repo;
 import home.investigation.rrr.domain.Tour;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
